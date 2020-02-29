a42e13d53a5b26acdfa68b7753dcdf421ba9f4ce
Lucas Scheinkerman <lscheink@creativa77.com.ar>
2020-02-29 00:54:33 +0000

    First implementation of Matrix4.

---
+++ course/include/isometry.h
@@ -0,0 +1,152 @@
+#ifndef ISOMETRY_H
+#define ISOMETRY_H
+
+#include <cstdint>
+#include <iostream>
+#include <ostream>
+
+namespace ekumen {
+namespace math {
+
+class Vector4{
+    public:
+        // Constructors
+        Vector4(double x = 0, double y = 0, double z = 0) : x_{x}, y_{y}, z_{z}, w_{w} {}
+        Vector4(const Vector4& v) : Vector4(v.x_, v.y_, v.z_, v.w_) {}
+        Vector4(const std::initializer_list<double>& l) {
+            if(l.size() != 3) {
+                throw "Invalid initializer list size";
+            }
+            std::initializer_list<double>::iterator it = l.begin();
+            x_ = *it++;
+            y_ = *it++;
+            z_ = *it++;
+            w_ = *it;
+        }
+
+        // Operators
+        double& operator [] (const uint8_t);
+        const double& operator [] (const uint8_t) const;
+        Vector4 operator + (const Vector4&) const;
+        Vector4 operator - (const Vector4&) const;
+        Vector4 operator * (const Vector4&) const;
+        Vector4 operator * (const double&) const;
+        Vector4 operator / (const Vector4&) const;
+        Vector4& operator = (const Vector4&);
+        Vector4& operator += (const Vector4&);
+        Vector4& operator -= (const Vector4&);
+        Vector4& operator *= (const Vector4&);
+        Vector4& operator /= (const Vector4&);
+        bool operator == (const Vector4&) const;
+        bool operator != (const Vector4&) const;
+        bool operator == (const std::initializer_list<double>& list) const;
+        bool operator != (const std::initializer_list<double>& list) const;
+
+        friend Vector4 operator * (const double d, const Vector4& v) {
+            return Vector4(v.x_ * d, v.y_ * d, v.z_ * d, v.w_ * d);
+        }
+
+        friend std::ostream& operator << (std::ostream& os, const Vector4& v) {
+            os << std::string("(x: ") << v.x() << ", y: " << v.y() << ", z: " << v.z() << ", w: " << v.w() << ")";
+            return os;
+        }
+
+        // Getters
+        const double& x() const {return x_;};
+        const double& y() const {return y_;};
+        const double& z() const {return z_;};
+        const double& w() const {return w_;};
+
+        // Setters
+        double& x() {return x_;};
+        double& y() {return y_;};
+        double& z() {return z_;};
+        double& w() {return w_;};
+
+        // Computations
+        double dot(const Vector4&) const;
+        double norm() const;
+
+        // Class constants
+        static const Vector4 kUnitX;
+        static const Vector4 kUnitY;
+        static const Vector4 kUnitZ;
+        static const Vector4 kUnitW;
+        static const Vector4 kZero;
+
+    private:
+        // Attributes
+        double x_;
+        double y_;
+        double z_;
+        double w_;
+};
+
+class Matrix4{
+    public:
+        // Constructors
+        Matrix4(const Vector4& r1 = Vector4(), 
+                const Vector4& r2 = Vector4(), 
+                const Vector4& r3 = Vector4(),
+                const Vector4& r4 = Vector4())
+                 : r1_{r1}, r2_{r2}, r3_{r3}, r34_{r4} {}
+        Matrix4(const Matrix4& m) : Matrix4(m.row(0), m.row(1), m.row(2), m.row(3)) {}
+        Matrix4(const std::initializer_list<double>& list);
+        Matrix4(const std::initializer_list<double>&,
+                const std::initializer_list<double>&,
+                const std::initializer_list<double>&,
+                const std::initializer_list<double>&);
+        Matrix4(Matrix4&& m) : Matrix4(m.row(0), m.row(1), m.row(2), m.row(3)){}
+
+        // Operators
+        Vector4& operator [] (const uint8_t);
+        const Vector4& operator [] (const uint8_t) const;
+        Matrix4 operator + (const Matrix4&) const;
+        Matrix4 operator - (const Matrix4&) const;
+        Matrix4 operator * (const Matrix4&) const;
+        Matrix4 operator * (const double) const;
+        Matrix4 operator / (const Matrix4&) const;
+        Matrix4& operator = (const Matrix4&);
+        Matrix4& operator += (const Matrix4&);
+        Matrix4& operator -= (const Matrix4&);
+        Matrix4& operator *= (const Matrix4&);
+        Matrix4& operator /= (const Matrix4&);
+        bool operator == (const Matrix4&);
+        bool operator != (const Matrix4&);
+        Vector4 operator * (const Vector4&);
+        Matrix4& operator = (Matrix4&&);
+
+        friend Matrix4 operator * (const double d, const Matrix4& m) {
+                return Matrix4(m.r1_ * d, m.r2_ * d, m.r3_ * d);
+        }
+
+        friend std::ostream& operator << (std::ostream& os, const Matrix4& m) {
+            os << std::string("[");
+            for(int i = 0; i < 3; i++) {
+                os << m.row(i);
+            }
+            return os;
+        }
+
+        // Getters
+        Vector4 row(uint8_t index) const;
+        Vector4 col(uint8_t index) const;
+
+        // Computations
+        double det() const;
+
+        // Class constants
+        static const Matrix4 kIdentity;
+        static const Matrix4 kZero;
+        static const Matrix4 kOnes;
+
+    private:
+        // Ordered by rows.
+        Vector4 r1_, r2_, r3_;
+
+};
+
+}  // namespace math
+}  // namespace ekumen
+
+#endif  // ISOMETRY_H
---
+++ course/src/isometry.cc
@@ -0,0 +1,313 @@
+#include <cmath>
+#include <cstdint>
+#include "isometry.h"
+
+namespace ekumen {
+namespace math {
+
+const double& Vector4::operator [] (const uint8_t index) const {
+    if(index > 2) {
+        throw "Invalid index number";
+    }
+    switch(index) {
+        case 0:
+            return x_;
+        case 1:
+            return y_;
+        case 2:
+            return z_;
+        case 3:
+            return w_;
+    }
+}
+
+double& Vector4::operator [] (const uint8_t index) {
+    if(index > 2) {
+        throw "Invalid index number";
+    }
+    switch(index) {
+        case 0:
+            return x_;
+        case 1:
+            return y_;
+        case 2:
+            return z_;
+        case 3:
+            return w_;
+    }
+}
+
+Vector4 Vector4::operator + (const Vector4& v) const {
+    return Vector4(x_ + v.x_, y_ + v.y_, z_ + v.z_, w_ + v.w_);
+}
+
+Vector4 Vector4::operator - (const Vector4& v) const {
+    return Vector4(x_ - v.x_, y_ - v.y_, z_ - v.z_, w_ - v.w_);
+}
+
+Vector4 Vector4::operator * (const Vector4& v) const {
+    return Vector4(x_ * v.x_, y_ * v.y_, z_ * v.z_, w_ * v.w_);
+}
+
+Vector4 Vector4::operator * (const double& d) const {
+    return Vector4(x_ * d, y_ * d, z_ * d, w_ * d);
+}
+
+Vector4 Vector4::operator / (const Vector4&v) const {
+    return Vector4(x_ / v.x_, y_ / v.y_, z_ / v.z_, w_ / v.w_);
+}
+
+Vector4& Vector4::operator = (const Vector4& v) {
+    x_ = v.x();
+    y_ = v.y();
+    z_ = v.z();
+    w_ = v.w();
+    return *this;
+}
+
+Vector4& ekumen::math::Vector4::operator += (const Vector4& v) { 
+    x_ += v.x_;
+    y_ += v.y_;
+    z_ += v.z_;
+    w_ += v.w_;
+    return *this;
+}
+
+Vector4& ekumen::math::Vector4::operator -= (const Vector4& v) {
+    x_ -= v.x_;
+    y_ -= v.y_;
+    z_ -= v.z_;
+    w_ -= v.w_;
+    return *this; 
+}
+
+Vector4& ekumen::math::Vector4::operator *= (const Vector4& v) {
+    x_ *= v.x_;
+    y_ *= v.y_;
+    z_ *= v.z_;
+    w_ *= v.w_;
+    return *this; 
+}
+
+Vector4& ekumen::math::Vector4::operator /= (const Vector4& v) {
+    x_ /= v.x_;
+    y_ /= v.y_;
+    z_ /= v.z_;
+    w_ /= v.w_;
+    return *this; 
+}
+
+bool ekumen::math::Vector4::operator == (const Vector4& v) const {
+    return (x_ == v.x_) && (y_ == v.y_) && (z_ == v.z_) && (w_ == v.w_);
+}
+
+bool ekumen::math::Vector4::operator != (const Vector4& v) const {
+    return !(*this == v);
+}
+
+bool ekumen::math::Vector4::operator == (const std::initializer_list<double>& list) const {
+    if(list.size() != 4) {
+        throw "Invalid initializer list size";
+    }
+
+    return (*this) == Vector4(list);
+}
+
+bool ekumen::math::Vector4::operator != (const std::initializer_list<double>& list) const {
+    return !(*this == list);
+}
+
+// Computations
+double Vector4::dot(const Vector4& v) const {
+    return (x_ * v.x_) + (y_ * v.y_) + (z_ * v.z_) + (w_ * v.w_);
+}
+
+double Vector4::norm() const {
+    return sqrt( (x_ * x_) + (y_ * y_) + (z_* z_) + (w_* w_));
+}
+
+// Class constants
+const Vector4 Vector4::kUnitX = Vector4(1, 0, 0, 0);
+const Vector4 Vector4::kUnitY = Vector4(0, 1, 0, 0);
+const Vector4 Vector4::kUnitZ = Vector4(0, 0, 1, 0);
+const Vector4 Vector4::kUnitW = Vector4(0, 0, 1, 0);
+const Vector4 Vector4::kZero = Vector4(0, 0, 0, 0);
+
+// Constructors
+Matrix4::Matrix4(const std::initializer_list<double>& list) {
+    if(list.size() != 16) {
+        throw "Invalid initializer list size";
+    }
+    std::initializer_list<double>::iterator it = list.begin();
+
+    r1_[0] = *it++;
+    r1_[1] = *it++;
+    r1_[2] = *it++;
+    r1_[3] = *it++;
+
+    r2_[0] = *it++;
+    r2_[1] = *it++;
+    r2_[2] = *it++;
+    r2_[3] = *it++;
+
+    r3_[0] = *it++;
+    r3_[1] = *it++;
+    r3_[2] = *it++;
+    r3_[3] = *it++;
+
+    r4_[0] = *it++;
+    r4_[1] = *it++;
+    r4_[2] = *it++;
+    r4_[3] = *it;
+}
+
+Matrix4::Matrix4(const std::initializer_list<double>& l1,
+                 const std::initializer_list<double>& l2,
+                 const std::initializer_list<double>& l3,
+                 const std::initializer_list<double>& l4) {
+    r1_ = Vector4(l1);
+    r2_ = Vector4(l2);
+    r3_ = Vector4(l3);
+    r4_ = Vector4(l4);
+}
+
+Matrix4(Matrix4&& m) {
+    r1_ = m.r1_;
+    r2_ = m.r2_;
+    r3_ = m.r3_;
+    r4_ = m.r4_;
+}
+
+// Operators
+Vector4& Matrix4::operator [] (const uint8_t index) {
+    return this->row(index);
+}
+
+const Vector4& Matrix4::operator [] (const uint8_t index) const {
+    return this->row(index);
+}
+
+Matrix4 Matrix4::operator + (const Matrix4& m) const {
+    return Vector4(x_ + v.x_, y_ + v.y_, z_ + v.z_ + v.w_);
+}
+
+Matrix4 Matrix4::operator - (const Matrix4& m) const {
+    return Vector4(x_ - v.x_, y_ - v.y_, z_ - v.z_, w_ - v.w_);
+}
+
+Matrix4 Matrix4::operator * (const Matrix4& m) const {
+   return Vector4(x_ * v.x_, y_ * v.y_, z_ * v.z_, w_ * v.w_);
+}
+
+Matrix4 Matrix4::operator * (const double d) const {
+    return Matrix4(r1_ * d, r2_ * d, r3_ * d, r4_ * d);
+}
+
+Matrix4 Matrix4::operator / (const Matrix4& m) const {
+    return Matrix4(r1_ / m.r1_, r2_ / m.r2_, r3_ / m.r3_, r4_ / m.r4_);
+}
+
+Matrix4& Matrix4::operator = (const Matrix4& m) {
+    r1_ = m.r1_;
+    r2_ = m.r2_;
+    r3_ = m.r3_;
+    r4_ = m.r4_;
+    return *this;
+}
+
+Matrix4& Matrix4::operator += (const Matrix4& m) {
+    r1_ += m.r1_;
+    r2_ += m.r2_;
+    r3_ += m.r3_;
+    r4_ += m.r4_;
+    return *this;
+}
+
+Matrix4& Matrix4::operator -= (const Matrix4& m) {
+    r1_ -= m.r1_;
+    r2_ -= m.r2_;
+    r3_ -= m.r3_;
+    r4_ -= m.r4_;
+    return *this;
+}
+
+Matrix4& Matrix4::operator *= (const Matrix4& m) {
+    r1_ *= m.r1_;
+    r2_ *= m.r2_;
+    r3_ *= m.r3_;
+    r4_ *= m.r4_;
+    return *this;
+}
+
+Matrix4& Matrix4::operator /= (const Matrix4& m) {
+    r1_ /= m.r1_;
+    r2_ /= m.r2_;
+    r3_ /= m.r3_;
+    r4_ /= m.r4_;
+    return *this;
+}
+
+bool Matrix4::operator == (const Matrix4& m) {
+    if ((r1_ == m.r1_) && (r2_ == m.r2_) && (r3_ == m.r3_) && (r3_ == m.r3_)) {
+        return true;
+    }
+    return false;
+}
+
+bool Matrix4::operator != (const Matrix4& m) {
+    return !(*this == m);
+}
+
+Vector4 Matrix4::operator * (const Vector4& v) {
+    return Vector4(r1_.dot(v), r2_.dot(v), r3_.dot(v), r4_.dot(v));
+}
+
+Matrix4& operator = (Matrix4&& m) {
+    r1_ = std::move(m.r1_);
+    r2_ = std::move(m.r2_);
+    r3_ = std::move(m.r3_);
+    r4_ = std::move(m.r4_);
+
+    return *this; // What about the original matrix? (m) It shouldn't have its rows available, if I'm not mistaken
+}
+
+// Getters
+Vector4 Matrix4::row(uint8_t index) const {
+    switch(index) {
+        case 0:
+            return r1_;
+        case 1:
+            return r2_;
+        case 2:
+            return r3_;
+        case 3:
+            return r4_;
+        default:
+            throw "Error. Invalid row index for Matrix4";
+    }
+}
+
+Vector4 Matrix4::col(uint8_t index) const {
+    if(index > 3) {
+        throw "Error. Invalid column index for Matrix4";
+    }
+    return Vector4(r1_[index], r2_[index], r3_[index], r4_[index]);
+}
+
+// Computations
+double Matrix4::det() const {
+    // Implement
+}
+
+// Class constants
+const Matrix4 Matrix4::kIdentity = 
+    Matrix4(Vector4(1, 0, 0, 0), Vector4(0, 1, 0, 0),
+            Vector4(0, 0, 1, 0), Vector4(0, 0, 0, 1));
+
+const Matrix4 Matrix4::kZero = Matrix4();
+
+const Matrix4 Matrix4::kOnes = 
+    Matrix4(Vector4(1, 1, 1 , 1), Vector4(1, 1, 1, 1),
+            Vector4(1, 1, 1, 1), Vector4(1, 1, 1, 1));
+}  // namespace math
+}  // namespace ekumen
