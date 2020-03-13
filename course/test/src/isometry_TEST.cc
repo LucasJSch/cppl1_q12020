27836ce42d75c7728a520267eae0d48a3661660f
Lucas J. Scheinkerman <lucas.scheinkerman@gmail.com>
2020-03-13 16:59:39 -0300

    Fixed initializer list issue from isometry_TEST.cpp.
    Changed test CMakeLists for building correctly.

--- course/test/src/isometry_TEST.cc
+++ course/test/src/isometry_TEST.cc
@@ -21,8 +21,8 @@ GTEST_TEST(Vector3Test, Vector3Operations) {
   const Vector3 p{1., 2., 3.};
   const Vector3 q{4., 5., 6.};
 
-  EXPECT_EQ(p + q, {5., 7., 9.});
-  EXPECT_EQ(p - q, {-3., -3., -3.});
+  EXPECT_EQ(p + q, std::initializer_list<double>({5., 7., 9.}));
+  EXPECT_EQ(p - q, std::initializer_list<double>({-3., -3., -3.}));
   EXPECT_EQ(p * 2., Vector3(2., 4., 6));
   EXPECT_EQ(2 * q, Vector3(8., 10., 12.));
   EXPECT_EQ(p * q, Vector3(4., 10., 18.));
@@ -40,8 +40,8 @@ GTEST_TEST(Vector3Test, Vector3Operations) {
   EXPECT_EQ(ss.str(), "(x: 1, y: 2, z: 3)");
 
   EXPECT_TRUE(Vector3::kUnitX == Vector3(1., 0., 0));
-  EXPECT_TRUE(Vector3::kUnitX != {1., 1., 0});
-  EXPECT_TRUE(Vector3::kUnitY == {0., 1., 0});
+  EXPECT_TRUE(Vector3::kUnitX != std::initializer_list<double>({1., 1., 0}));
+  EXPECT_TRUE(Vector3::kUnitY == std::initializer_list<double>({0., 1., 0}));
   EXPECT_TRUE(Vector3::kUnitZ == Vector3::kUnitX.cross(Vector3::kUnitY));
   EXPECT_NEAR(Vector3::kUnitX.dot(Vector3::kUnitZ), 0., kTolerance);
 
@@ -63,7 +63,7 @@ GTEST_TEST(Matrix3Test, Matrix3Operations) {
   EXPECT_EQ(m1 - m2, Matrix3::kZero);
   EXPECT_EQ(m1 + m2, m1 * 2.);
   EXPECT_EQ(m1 + m2, 2. * m2);
-  EXPECT_EQ(m1 * m2, {1.,  4.,  9., 16., 25., 36., 49., 64., 81.});
+  EXPECT_EQ(m1 * m2, std::initializer_list<double>({1.,  4.,  9., 16., 25., 36., 49., 64., 81.}));
   EXPECT_EQ(m1 / m2, Matrix3::kOnes);
   EXPECT_NEAR(m1.det(), 0., kTolerance);
   m1[2][2] = 10.;
